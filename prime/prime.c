76c921ffcde2a94b438e8ddec2b317051c736601
ZUMO <phornphatch@gmail.com>
2023-06-02 18:43:55 +0000

    Fri, Jun 2, 2023, 6:43 PM +00:00

--- prime/prime.c
+++ prime/prime.c
@@ -31,7 +31,10 @@ int main(void)
 bool prime(int number)
 {
     int n = 2
-    if (number % n == 0; n <= number)
+    if (number == 2) {
+        return true;
+    }
+    else if (number % n == 0; n <= number; n++)
     {
         return false
 
