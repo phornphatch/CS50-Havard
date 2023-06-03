c2ef3839f645e22d3bcf3af9c954f7b6fabd036c
ZUMO <phornphatch@gmail.com>
2023-06-03 11:59:20 +0000

    Sat, Jun 3, 2023, 2:59 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height >= 1 || height <= 8);
+
+    int i;
+    for (i = 1; i < height; i++)
+    {
+        printf("#");
+    }
 }
