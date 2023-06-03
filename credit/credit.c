4a28691e95ea77926252d5b36756a731341979f4
ZUMO <phornphatch@gmail.com>
2023-06-03 13:52:20 +0000

    Sat, Jun 3, 2023, 4:52 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,14 +7,16 @@ int main(void)
     long number = get_long("Number: ");
 
     // calculate checksum
-    for (int i=10; i<;)
+    int i;
+    while ()
+    {
     long last_number = number % 10;
-    int i = 0;
     if (last_number < 10) {
         printf("last number is, %li", last_number);
         number = number / 10;
     }
     printf("remaining number is, %li", number);
+    }
 
     // check card length
 
