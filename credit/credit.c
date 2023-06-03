03e1f0c545ee42ac14d45ca9b2b772d0ee4ae560
ZUMO <phornphatch@gmail.com>
2023-06-03 14:01:19 +0000

    Sat, Jun 3, 2023, 5:01 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,15 +8,17 @@ int main(void)
 
     // calculate checksum
     int i = 10;
+    int loop_count = 1;
     while (number % i < number)
     {
-    long last_number = number % i;
-    if (last_number < i) {
-        printf("last number is, %li\n", last_number);
-        number = number / i;
-    }
-    printf("remaining number is, %li\n", number);
+        long last_number = number % i;
+        if (last_number < i) {
+            printf("last number is, %li\n", last_number);
+            number = number / i;
+        }
+        printf("remaining number is, %li\n", number);
         printf("---\n");
+        loop_count++;
     }
 
     // check card length
