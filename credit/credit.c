2396c40ea04eb145488d498dd5c81ec67e437b9f
ZUMO <phornphatch@gmail.com>
2023-06-03 13:42:20 +0000

    Sat, Jun 3, 2023, 4:42 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,15 +10,16 @@ int main(void)
     char str[20];
     sprintf(str, "%ld", number);
 
-
+    int sum1 = 0;
     // calculate checksum
     for (int i=strlen(str); i >= 0; i-=2)
     {
 
         printf("%c ", str[i]);
         int a = atoi(str[i]);
-
+        sum1 += a;
     }
+    printf("sum1: %i)
 
     // check card length
 
