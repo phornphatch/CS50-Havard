7e628493ab14d24abb37ab9b1eeea37fe3b7d964
ZUMO <phornphatch@gmail.com>
2023-06-11 16:55:46 +0000

    Sun, Jun 11, 2023, 7:55 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -106,6 +106,8 @@ int main(int argc, string argv[])
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
 
+        
+
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
 
