d91023cb9690824f2a41bd11e5506da89015faad
ZUMO <phornphatch@gmail.com>
2023-06-11 10:23:06 +0000

    Sun, Jun 11, 2023, 1:23 PM +03:00

--- hours/hours.c
+++ hours/hours.c
@@ -27,5 +27,8 @@ int main(void)
 // TODO: complete the calc_hours function
 float calc_hours(int hours[], int weeks, char output)
 {
-    
+     for (int i = 0; i < weeks; i++)
+    {
+        output += hours[i];
+    }
 }
