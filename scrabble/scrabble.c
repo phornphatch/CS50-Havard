232e24fd834152c3f84477d950979e7347bb60c5
ZUMO <phornphatch@gmail.com>
2023-06-11 12:08:32 +0000

    Sun, Jun 11, 2023, 3:08 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,5 +1,5 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -18,7 +18,19 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score1 = score2)
+    {
+        printf("Tie!");
+    }
+    else
+    {
+        printf("Player 2 wins!");
+    }
 }
 
 int compute_score(string word)
