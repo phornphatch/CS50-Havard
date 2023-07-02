ac613c351261fd18e0ba7ab4f3e385a9d3bb7029
ZUMO <phornphatch@gmail.com>
2023-07-02 15:52:40 +0000

    Sun, Jul 2, 2023, 6:52 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -107,7 +108,7 @@ bool vote(int rank, string name, int ranks[])
     bool voted = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name) == 0)
+        if (strcmp(name, candidates[i]) == 0)
         {
             ranks[i] += 1;
             voted = true;
