41abde52900caab5e9ecda039a87e6938d67e51b
ZUMO <phornphatch@gmail.com>
2023-07-04 16:10:23 +0000

    Tue, Jul 4, 2023, 7:10 PM +03:00

--- pdf/pdf.c
+++ pdf/pdf.c
@@ -12,7 +12,7 @@ int main(int argc, string argv[])
     }
 
     // open file
-    string filename = argv[1];         // ดึงเอา 2nd argument
+    string filename = argv[1];        // ดึงเอา 2nd argument
     FILE *file = fopen(argv[1], "r"); // r is reading mode
 
     // check if file exists
@@ -23,16 +23,23 @@ int main(int argc, string argv[])
     }
 
     uint8_t buffer[4];
-    // uint8_t is special type of 'pdf'.
-    // uint = unsigned integer. 8 = 8 bits or single byte. _t = all type
-    // หมายถึง unsigned interger ทั้งหมด
-    // 4 เพราะ pdf จะขึ้นด้วยเลข 4 ตัว 37 80 68 70
+    uint8_t signature[] = {37, 80, 68, 70}; // uint8_t is special type of 'pdf'.
+                                           // uint = unsigned integer. 8 = 8 bits or single byte. _t = all type
+                                           // หมายถึง unsigned interger ทั้งหมด
+                                           // 4 เพราะ pdf จะขึ้นด้วยเลข 4 ตัว 37 80 68 70
 
     fread(buffer, 1, 4, file); // (location, size of block to read, how many block to read, location to read from)
 
     for (int i = 0; i < 4; i++)
     {
-        printf("%i\n", buffer[i]);
+        // Check dose the buffer signer match?
+        if (buffer[i] != signature[i])
+        {
+            printf("It's not pdf.\n");
+            return 0;
+        }
+        printf("It's a  pdf !!!!\n");
+        return 1;
     }
 
     fclose(file);
