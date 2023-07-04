a4a26aab6562ca4f6db15ff48b2daa94895996f7
ZUMO <phornphatch@gmail.com>
2023-07-04 15:57:21 +0000

    Tue, Jul 4, 2023, 6:57 PM +03:00

--- pdf/pdf.c
+++ pdf/pdf.c
@@ -1,10 +1,26 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int main(int argc, string argv[])
 {
+
+    if (argc != 2) // check : should have 2nd argument
+    {
+        printf("add more argc\n");
+        return 1;
+    }
+
+    // open file
+    string filename = argc[1];         // ดึงเอา 2nd argument
     FILE *input = fopen(argv[1], "r"); // r is reading mode
+
+    // check if file exists
+    if (file == NULL)
+    {
+        printf("No such file found.\n");
+        return 1;
+    }
 
     uint8_t buffer[4];
 
