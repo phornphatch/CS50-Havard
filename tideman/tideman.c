ca80b1db41f78a29655ec2c70bd139bf5747490e
ZUMO <phornphatch@gmail.com>
2023-07-06 16:37:37 +0000

    Thu, Jul 6, 2023, 7:37 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -226,9 +226,14 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
-    return;
     // Update 'locked' to create the locked graph by adding all edges in decreasing order of victory strength, as long as there is
     // no cycle
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+        }
+    }
 }
 
 // Print the winner of the election
