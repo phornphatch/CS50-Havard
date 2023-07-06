7c91d55ca66e51d1efef7bc3ef7e0efa3b9c7369
ZUMO <phornphatch@gmail.com>
2023-07-06 14:38:28 +0000

    Thu, Jul 6, 2023, 5:38 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -129,7 +129,7 @@ void record_preferences(int ranks[])
     // i j คือดำแหน่งในตาราง
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             // ต้องเชคก่อนว่าใครอยุก่อน
             if (i != j)
