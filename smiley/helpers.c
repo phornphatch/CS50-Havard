42d1d4ec82663ac3fac06714f8d3252caf8a89d7
ZUMO <phornphatch@gmail.com>
2023-07-09 14:53:13 +0000

    Sun, Jul 9, 2023, 5:53 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -8,8 +8,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if (image[i][i].rgbtRed == 0x00 && image[i][i].rgbtGreen == 0x00 && image[i][i].rgbtBlue == 0x00)
-            image[i][i].rgbtRed == 0x00 && image[i][i].rgbtGreen == 0x00 && image[i][i].rgbtBlue == 0x00)
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            image[i][j].rgbtRed = 0x25;
+            image[i][j].rgbtGreen = 0x13;
+            image[i][j].rgbtBlue = 0x51;
         }
     }
 }
