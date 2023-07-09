ec7a0d613d6f25bef05a9d46f238f582c8032c19
ZUMO <phornphatch@gmail.com>
2023-07-09 14:49:17 +0000

    Sun, Jul 9, 2023, 5:49 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,6 +1,15 @@
 #include "helpers.h"
+#include "stdio.h"
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][i].rgbtRed == 0x00 && image[i][i].rgbtGreen == 0x00 && image[i][i].rgbtBlue == 0x00)
+            image[i][i].rgbtRed == 0x00 && image[i][i].rgbtGreen == 0x00 && image[i][i].rgbtBlue == 0x00)
+        }
+    }
 }
