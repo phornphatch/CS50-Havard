9a65414cb9944c01257a41b3f07ca1aa1aaaf68a
ZUMO <phornphatch@gmail.com>
2023-07-09 16:19:38 +0000

    Sun, Jul 9, 2023, 7:19 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -27,25 +27,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-     // Create array to store plate numbers
-    char *plates[8];
-
-    // memory allocation
-    for (int i = 0; i <= 8; i++)
-    {
-        plates[i] = malloc(7);
-        if (plates[i] == NULL) // ถ้า mem ไม่พอ
-        {
-            return 1;
-        }
-    }
 
     for (int i = 0; i <= height; i++) // access to each row
     {
         // switch (mirror)
-        for (int j = 0; i <= width; i++)
+        for (int j = 0; j <= width; j++)
         {
-             strcpy(plates[idx], buffer);
             image[i][j].rgbtRed = image[i][width - j].rgbtRed;
             image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
             image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
