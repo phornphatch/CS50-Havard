dba46841704f8bc81b79e23db30a753525306a1b
ZUMO <phornphatch@gmail.com>
2023-07-10 17:32:43 +0000

    Mon, Jul 10, 2023, 8:32 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -17,11 +17,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             int num_green = image[i][j].rgbtGreen;
             int num_blue = image[i][j].rgbtBlue;
             double num_avg = ((num_red + num_green + num_blue) / 3);
-            // printf("%d", num_avg);
-            printf("%f", num_avg);
+
             image[i][j].rgbtRed = round(num_avg);
             image[i][j].rgbtGreen = round(num_avg);
             image[i][j].rgbtBlue = round(num_avg);
+
+            if(i==0 && j==0)
+            {
+                 printf("%f", num_avg);
+                 printf("%i", num_red);
+printf("%i", num_green);printf("%i", num_blue);
+            }
         }
     }
 }
