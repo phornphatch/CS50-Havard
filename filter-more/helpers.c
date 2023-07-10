045c7f6c2cdc0979d39e9b7020b7d52e087134b5
ZUMO <phornphatch@gmail.com>
2023-07-10 19:45:50 +0000

    Mon, Jul 10, 2023, 10:45 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -233,19 +233,19 @@ GALL find_g(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE ori
     final_value.GGreen = sqrt((gx_green * gx_green) + (gy_green * gy_green));
     final_value.GBlue = sqrt((gx_blue * gx_blue) + (gy_blue * gy_blue));
 
-    if (final_value.GRed > 225)
+    if (final_value.GRed > 255)
     {
-        final_value.GRed = 225;
+        final_value.GRed = 255;
     }
 
-     if (final_value.GGreen > 225)
+     if (final_value.GGreen > 255)
     {
-        final_value.GGreen = 225;
+        final_value.GGreen = 255;
     }
 
-     if (final_value.GBlue > 225)
+     if (final_value.GBlue > 255)
     {
-        final_value.GBlue = 225;
+        final_value.GBlue = 255;
     }
 
     return final_value;
