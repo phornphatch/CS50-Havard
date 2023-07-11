02b5cdc79cc9397ddfd5e70829145d061f167c0a
ZUMO <phornphatch@gmail.com>
2023-07-11 14:39:23 +0000

    Tue, Jul 11, 2023, 5:39 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,35 +25,32 @@ int main(int argc, char *argv[])
     typedef uint8_t BYTE;
     BYTE buffer[512];
 
-    fread(buffer, 4, 1, file); // (location, size of block to read, how many block to read, location to read from)
-
     // Check buffer match with JPEG?
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
-{
-
-
-}
-    if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] & 0xf0) == 0xe0)
+    while (fread(buffer, 512, 1, raw_file) ==
+           BLOCK_SIZE) // (location, size of block to read, how many block to read, location to read from)
     {
-        printf("It's jpeg.\n");
+        if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] & 0xf0) == 0xe0)
+        {
+            printf("It's jpeg.\n");
+            return 0;
+        }
+        else
+        {
+            printf("It's not a  jpeg !!!!\n");
+        }
         return 0;
     }
-    else
-    {
-        printf("It's not a  jpeg !!!!\n");
-    }
-    return 0;
     fclose(file); // ****** ถ้าไม่ close จะ leak memory ถ้า run valgrind ./pdf test.pdf ดู
-     // --- end of check: JPEG? ---
+                  // --- end of check: JPEG? ---
 
     // open new jpeg file
 
-        // Create a new block of memory to store filename
-        int filename_length = 3;
-        char *filename = malloc(sizeof(char) * (filename_length + 1)); // ### + \0
+    // Create a new block of memory to store filename
+    int filename_length = 3;
+    char *filename = malloc(sizeof(char) * (filename_length + 1)); // ### + \0
 
-        // Copy argv[1] into block of memory for filename
-        sprintf(filename, "%03i.jpg", 2);
+    // Copy argv[1] into block of memory for filename
+    sprintf(filename, "%03i.jpg", 2);
 
     // sprintf(filename, "%03i.jpg", 2);
     // FILE *img = fopen(filename, "w");
