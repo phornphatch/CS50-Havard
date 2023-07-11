305bad35b862c6b7d29964de3b9b844a8f000df9
ZUMO <phornphatch@gmail.com>
2023-07-11 12:53:38 +0000

    Tue, Jul 11, 2023, 3:53 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,12 +1,28 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
- // open memory card
- FILE *f = fopen(filename, 'r')
- // look for beginning of jpeg
- // open new jpeg file
- // write 512 bytes until a new jpeg is found
 
+    if (argc != 2) // check : should have 2nd argument
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    // open memory card
+    char* filename = argv[1];        // ดึงเอา 2nd argument
+    FILE *file = fopen(argv[1], "r"); // r is reading mode
+
+      // check if file exists
+    if (file == NULL)
+    {
+        printf("No such file found.\n");
+        return 1;
+    }
+    
+    // look for beginning of jpeg
+    // open new jpeg file
+    // write 512 bytes until a new jpeg is found
 }
