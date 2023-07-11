1a1356942e27c38bbd4721163ad8af51840e9f46
ZUMO <phornphatch@gmail.com>
2023-07-11 12:49:34 +0000

    Tue, Jul 11, 2023, 3:49 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
+ // open memory card
+ // look for beginning of jpeg
+ // open new jpeg file
+ // write 512 bytes until a new jpeg is found
  
 }
