d3e84d18103ce2fa7603fc37a5757f5c24f8aad0
ZUMO <phornphatch@gmail.com>
2023-07-11 12:50:37 +0000

    Tue, Jul 11, 2023, 3:50 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,8 +4,9 @@
 int main(int argc, char *argv[])
 {
  // open memory card
+ FILE *f = fopen(filename, 'r')
  // look for beginning of jpeg
  // open new jpeg file
  // write 512 bytes until a new jpeg is found
- 
+
 }
