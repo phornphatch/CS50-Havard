09ec676b9eaf558e4b363638bb5a8e0a953d6360
ZUMO <phornphatch@gmail.com>
2023-07-11 16:26:04 +0000

    Tue, Jul 11, 2023, 7:26 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,6 @@ int main(int argc, char *argv[])
     // Check buffer match with JPEG?
     while (fread(buffer, sizeof(BYTE)*512, 1, raw_file) == 1) // (location, size of block to read, how many block to read, location to read from)
     {
-        printf("not img yet\n");
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             if (jpegStarted == true)
@@ -77,6 +76,10 @@ int main(int argc, char *argv[])
                 // fclose(img);
             }
         }
+    }
+    if (img != NULL)
+    {
+        fclose(img)
     }
     fclose(raw_file); // ****** ถ้าไม่ close จะ leak memory ถ้า run valgrind ./pdf test.pdf ดู
     free(filename);
