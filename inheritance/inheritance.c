2df39b450d9002694e645bfe0067274bdbff9c4a
ZUMO <phornphatch@gmail.com>
2023-07-18 13:44:20 +0000

    Tue, Jul 18, 2023, 4:44 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -61,8 +61,11 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-
+        new_person->parent[0] = NULL;
+        new_person->parent[1] = NULL;
         // TODO: Randomly assign alleles
+        new_person->allels[0] = random_allele();
+        new_person->allels[1] = random_allele();
     }
 
     // TODO: Return newly created person
