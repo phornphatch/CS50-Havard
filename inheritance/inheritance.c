be858690e4a8a7cb645021ee1fc99148cabd9b75
ZUMO <phornphatch@gmail.com>
2023-07-18 13:03:33 +0000

    Tue, Jul 18, 2023, 4:03 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person = malloc(sizeof(person));
+    person = malloc(generations - 1);
 
     // If there are still generations left to create
     if (generations > 1)
