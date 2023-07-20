7fbcfa7c0e473026bc2b014a9c9f70d3fcd77505
ZUMO <phornphatch@gmail.com>
2023-07-20 14:18:44 +0000

    Thu, Jul 20, 2023, 5:18 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -57,8 +57,6 @@ bool load(const char *dictionary)
         printf("No such file found.\n");
         return 1;
     }
-    // allowcate memory
-    
     // read strings from file one at a time
      fread(buffer, 1, 4, file);
     //  use fscanf(file, %s , word)
