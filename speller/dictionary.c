bfde275b2608804a27afdf70b5b44ad64fe085dc
ZUMO <phornphatch@gmail.com>
2023-07-20 14:09:08 +0000

    Thu, Jul 20, 2023, 5:09 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -58,6 +58,8 @@ bool load(const char *dictionary)
         return 1;
     }
     // read strings from file one at a time
+     while (fread(buffer, 1, 7, infile) == 7) // อ่านไลฟล์ทีละ 7 ตัว
+    {
     //  use fscanf(file, %s , word)
     // fscan will return EOF ince it reaches end of file
     // create a new node for each word
