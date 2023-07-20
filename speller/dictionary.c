002a1dd5fb98201018265790be2465d74e3a7ae8
ZUMO <phornphatch@gmail.com>
2023-07-20 17:14:23 +0000

    Thu, Jul 20, 2023, 8:14 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -91,7 +91,6 @@ bool load(const char *dictionary)
         // create a new node for each word
         // use malloc
         node *new = malloc(sizeof(node));
-        new->next = NULL;
 
         num_word++;
         // remember to check if return value is null
