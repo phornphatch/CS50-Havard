b0439ce8d8fd344084c1dd735dfd6581d1bd7a9a
ZUMO <phornphatch@gmail.com>
2023-07-27 15:41:57 +0000

    Thu, Jul 27, 2023, 6:41 PM +03:00

--- src6/reads.py
+++ src6/reads.py
@@ -4,10 +4,12 @@ books = []
 
 # Add books to your shelf by reading from books.csv
 with open("books.csv") as file: # หมายความว่า open csv นั้น แล้วเรียกว่า file
-    file_reader = csv.DictReader(file)
+    file_reader = csv.DictReader(file) # จะใช้ reader ก้ได้ แต่มันจะออกหัวตารางมาด้วย ในที่นี้คือ "title, author"
     for book in file_reader:
         books.append(book) # append คือ adds an item to the end of the list.
 
 # Print titles of books
 for book in books:
     print(book["title"])
+
+# ไม่ต้อง close เพระา python ทำมาให้แล้ว
