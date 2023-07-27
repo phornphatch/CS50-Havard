cf786d86e1215b99fdaf1587b15152fc4c08a756
ZUMO <phornphatch@gmail.com>
2023-07-27 15:20:49 +0000

    Thu, Jul 27, 2023, 6:20 PM +03:00

--- src6/books.py
+++ src6/books.py
@@ -3,7 +3,7 @@ books = []
 # Add three books to your shelf
 for i in range(3):
     book = dict()
-    book["title"] = input("Title: ").lower().capitalize()
+    book["title"] = input("Title: ").strip().lower().capitalize() # ทำให้มันสวยขึ้น เรียบร้อยขึ้น
     book["author"] = input("Author: ")
 
     books.append(book) # insert book in the lists
