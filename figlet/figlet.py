e476fda62f82477c9722eddb81a6b918956da8af
ZUMO <phornphatch@gmail.com>
2023-07-30 12:28:13 +0000

    Sun, Jul 30, 2023, 3:28 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,9 +6,10 @@ figlet = Figlet()
 
 if len(argv) > 0:
     if argv[1] == '-f' or argv[1] == '--font':
+        font = argv[2]
         text = get_string("Input: ")
         print("Output: ")
-        figlet.setFont(argv[2])
+        figlet.setFont(font)
         print(figlet.renderText(text))
     else:
         print("Invalid usage")
