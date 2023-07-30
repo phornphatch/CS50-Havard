348558b8b9f1987893aa574bd2f02b43a7acce87
ZUMO <phornphatch@gmail.com>
2023-07-30 14:34:29 +0000

    Sun, Jul 30, 2023, 5:34 PM +03:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,6 +1,18 @@
-im
+menus = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
 
 try:
     item = input("Item: ")
+    if item in d
+        total =
 except EOFError:
-    ...
+    print("\n")
