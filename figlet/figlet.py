efa7cf245e477cc9095e7c8916979f8bce8f4b37
ZUMO <phornphatch@gmail.com>
2023-07-30 12:23:13 +0000

    Sun, Jul 30, 2023, 3:23 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,13 +2,13 @@ from pyfiglet import Figlet
 from cs50 import get_string
 from sys import argv
 
-text = get_string("input: ")
-
 if len(argv) > 2:
-    if argv[1] == '-f' or argv[2] == '--font'
-    print()
+    if argv[1] == '-f' or argv[1] == '--font'
+    text = get_string("Input: ")
+    print("Output: ")
+    new_text = text.setFont(argv[2])
+    print(new_text)
     else:
     print("Invalid usage")
-
 elif len(argv) == 2
 
