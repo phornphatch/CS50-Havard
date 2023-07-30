9bc4f0f68a1d3da6e4cd54f8810d1dc9e03fe56a
ZUMO <phornphatch@gmail.com>
2023-07-30 13:08:55 +0000

    Sun, Jul 30, 2023, 4:08 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,9 +1,9 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity < 0
+        if capacity < 0:
             raise ValueError("Invalid capacity")
-        self.capacity = capacity
-        self.size = 0
+        self._capacity = capacity
+        self._size = 0
 
     def __str__(self):
         return "🍪" * self.size
@@ -11,21 +11,28 @@ class Jar:
     def deposit(self, n):
         if n > self.capacity:
             raise ValueError("Exceed")
-        if self.size + n > self.capacity
+        if self.size + n > self.capacity:
             raise ValueError("Exceed")
         self.size += n
 
+
     def withdraw(self, n):
-        ...
+          if n > self.size:
+            raise ValueError("Not enough")
+          self.size -= n
 
     @property
     def capacity(self):
-        ...
+        return self._capacity
 
     @property
     def size(self):
-        ...
+        return self._size
+
 
 jar = Jar()
 jar.deposit(2)
+jar.withdraw(1)
+jar.capacity()
+jar.size()
 print(jar)
