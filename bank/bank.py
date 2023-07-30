22d7f9568166d8033f699945737e84b564c9c93f
ZUMO <phornphatch@gmail.com>
2023-07-30 12:03:15 +0000

    Sun, Jul 30, 2023, 3:03 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,6 +2,7 @@ from cs50 import get_string
 
 greeting = get_string("Greeting: ")
 
-if greeting[0] == "H":
-    for i in range(greeting)
-    print("20$")
+remove_prefix = greeting.removeprefix('Hello')
+
+if greeting != remove_prefix:
+    print("0$")
