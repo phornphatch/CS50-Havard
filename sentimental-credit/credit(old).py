ab3a860ab37932fd226a404a514fa1119fea9d9d
ZUMO <phornphatch@gmail.com>
2023-08-01 17:48:28 +0000

    Tue, Aug 1, 2023, 8:48 PM +03:00

---
+++ sentimental-credit/credit(old).py
@@ -0,0 +1,61 @@
+from cs50 import get_int
+
+credit_card_number = get_int("Number: ")
+number = credit_card_number
+first_number = 0
+two_first_number = 0
+sum = 0
+loop_count = 1
+
+while number % 10 < number:
+    last_number = number % 10
+    if last_number < 10:
+        number = number // 10 # this number will be remaining number
+
+    if loop_count % 2 == 0:
+        product_last_number = last_number * 2
+        if product_last_number > 10:
+            last_digit_of_product_last_number = product_last_number % 10
+            first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) // 10
+            sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
+        elif product_last_number == 10:
+            sum = sum + 1
+        else:
+            sum = sum + product_last_number
+    else:
+        sum += last_number
+
+    loop_count += 1
+    first_number = number
+
+if loop_count % 2 == 0:
+    product_last_number = last_number * 2
+    if product_last_number > 10:
+        last_digit_of_product_last_number = product_last_number % 10
+        first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) // 10
+        sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
+    elif product_last_number == 10:
+        sum = sum + 1
+    else:
+        sum = sum + number * 2
+else:
+    sum = sum + number
+
+# find 2 first digits
+two_first_number = int(str(credit_card_number)[:2])
+
+print(sum)
+# Check checksum is valid according to Luhn's Algorithm or not
+if sum % 10 != 0:
+    print("INVALID")
+# Check card length and two first number
+elif loop_count == 15 and (two_first_number == 34 or two_first_number == 37):
+    print("AMEX")
+elif loop_count == 16 and (50 < two_first_number < 56):
+    print("MASTERCARD")
+elif loop_count == 16 and first_number == 4:
+    print("VISA")
+elif loop_count == 13 and first_number == 4:
+    print("VISA")
+else:
+    print("INVALID")
