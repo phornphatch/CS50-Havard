e15dadfd011a27cd8c9444dcf93febfead3c458d
ZUMO <phornphatch@gmail.com>
2023-08-01 17:55:59 +0000

    Tue, Aug 1, 2023, 8:55 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,3 +3,22 @@ from cs50 import get_int
 credit_card_number = get_int("Number: ")
 credis_card_string = str(credit_card_number)
 number_length = len(credit_card_string)
+sum = 0
+
+for range(i, number_length, 2)
+    
+
+# Check checksum is valid according to Luhn's Algorithm or not
+if sum % 10 != 0:
+    print("INVALID")
+# Check card length and two first number
+elif loop_count == 15 and (two_first_number == 34 or two_first_number == 37):
+    print("AMEX")
+elif loop_count == 16 and (50 < two_first_number < 56):
+    print("MASTERCARD")
+elif loop_count == 16 and first_number == 4:
+    print("VISA")
+elif loop_count == 13 and first_number == 4:
+    print("VISA")
+else:
+    print("INVALID")
