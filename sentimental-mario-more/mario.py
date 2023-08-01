2091bd5384e7dc996c676b22faafe377a40f77e5
ZUMO <phornphatch@gmail.com>
2023-08-01 13:45:14 +0000

    Tue, Aug 1, 2023, 4:45 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -8,13 +8,15 @@ while height < 1 or height > 8:
     except ValueError:
         pass
 
+# range(start, stop, step)
 for i in range(1, height, 1):
-    print("")
+    for j in range(1, height - i, 1):
+    print(" ")
 
-    for j in range(i, 1):
-        print("#")
+    for k in range(1, i, 1):
+    print("#")
 
     print(" ")
 
-    for k in range(i, 1):
-        print("#")
+    for z in range(1, i, 1):
+    print("#")
