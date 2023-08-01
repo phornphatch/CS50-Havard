3b40369eb3b3425833ad8d47efbd6a6b418a79e4
ZUMO <phornphatch@gmail.com>
2023-08-01 14:23:07 +0000

    Tue, Aug 1, 2023, 5:23 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,34 +6,49 @@ number = credit_card_number
 while number % 10 < number:
     last_number = number % 10
     if last_number < 10:
-        number = number / i
+        number = number / 10
 
     if len(number) % 2 == 0:
         product_last_number = last_number * 2
-        if product_last_number > 10
+        if product_last_number > 10:
             last_digit_of_product_last_number = product_last_number % 10
             first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) / 10
             sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
-        elif product_last_number == 0
+        elif product_last_number == 0:
             sum = sum + 1
-        else
+        else:
             sum = sum + product_last_number
-    else
+    else:
         sum = sum + last_number
 
     first_number = number
 
     if len(number) % 2 == 0:
         product_last_number = last_number * 2
-        if product_last_number > 10
+        if product_last_number > 10:
             last_digit_of_product_last_number = product_last_number % 10
             first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) / 10
             sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
-        elif product_last_number == 10
+        elif product_last_number == 10:
             sum = sum + 1
-        else
+        else:
             sum = sum + number * 2
-    else
+    else:
         sum = sum + number
 
     two_first_number = (credit_card_number - (credit_card_number % pow(10, loop_count - 2))) / (pow(10, loop_count - 2));
+
+    # Check checksum is valid according to Luhn's Algorithm or not
+    if sum % 10 != 0:
+        print("INVALID")
+    # Check card length and two first number
+    elif loop_count == 15 and (two_first_number == 34 or two_first_number == 37):
+        print("AMEX")
+    elif loop_count == 16 and (50 < two_first_number < 56):
+        print("MASTERCARD")
+    elif loop_count == 16 and first_number == 4:
+        print("VISA")
+    elif loop_count == 13 and first_number == 4:
+        print("VISA")
+    else:
+        print("INVALID")
