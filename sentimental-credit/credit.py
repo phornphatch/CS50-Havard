8f5b8151d71cadda53e130be49972109542af8f7
ZUMO <phornphatch@gmail.com>
2023-08-01 14:31:23 +0000

    Tue, Aug 1, 2023, 5:31 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -25,32 +25,32 @@ while number % 10 < number:
     loop_count += 1
     first_number = number
 
-    if loop_count % 2 == 0:
-        product_last_number = last_number * 2
-        if product_last_number > 10:
-            last_digit_of_product_last_number = product_last_number % 10
-            first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) / 10
-            sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
-        elif product_last_number == 10:
-            sum = sum + 1
-        else:
-            sum = sum + number * 2
+if loop_count % 2 == 0:
+    product_last_number = last_number * 2
+    if product_last_number > 10:
+        last_digit_of_product_last_number = product_last_number % 10
+        first_digit_of_product_last_number = (product_last_number - last_digit_of_product_last_number) / 10
+        sum = sum + last_digit_of_product_last_number + first_digit_of_product_last_number
+    elif product_last_number == 10:
+        sum = sum + 1
     else:
-        sum = sum + number
+        sum = sum + number * 2
+else:
+    sum = sum + number
 
-    two_first_number = (credit_card_number - (credit_card_number % pow(10, loop_count - 2))) / (pow(10, loop_count - 2));
+two_first_number = (credit_card_number - (credit_card_number % pow(10, loop_count - 2))) / (pow(10, loop_count - 2));
 
-    # Check checksum is valid according to Luhn's Algorithm or not
-    if sum % 10 != 0:
-        print("INVALID")
-    # Check card length and two first number
-    elif loop_count == 15 and (two_first_number == 34 or two_first_number == 37):
-        print("AMEX")
-    elif loop_count == 16 and (50 < two_first_number < 56):
-        print("MASTERCARD")
-    elif loop_count == 16 and first_number == 4:
-        print("VISA")
-    elif loop_count == 13 and first_number == 4:
-        print("VISA")
-    else:
-        print("INVALID")
+# Check checksum is valid according to Luhn's Algorithm or not
+if sum % 10 != 0:
+    print("INVALID")
+# Check card length and two first number
+elif loop_count == 15 and (two_first_number == 34 or two_first_number == 37):
+    print("AMEX")
+elif loop_count == 16 and (50 < two_first_number < 56):
+    print("MASTERCARD")
+elif loop_count == 16 and first_number == 4:
+    print("VISA")
+elif loop_count == 13 and first_number == 4:
+    print("VISA")
+else:
+    print("INVALID")
