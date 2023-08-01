6a08614bb3241f8342174340c5f4a1bca41ebd8c
ZUMO <phornphatch@gmail.com>
2023-08-01 18:09:15 +0000

    Tue, Aug 1, 2023, 9:09 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -10,8 +10,13 @@ i = 1
 j = 0
 
 for i in range(1, number_length, 2):
-    sum_even = int(credit_card_string[i])*2
-    
+    multiply = int(credit_card_string[i])*2
+    if multiply < 10:
+        sum_even += multiply
+    else:
+        multiply = str(multiply)
+        sum_even += (int(multiply[0]) + int(multiply[2]))
+
 
 for j in range(0, number_length, 2):
     sum_odd += int(credit_card_string[i])
