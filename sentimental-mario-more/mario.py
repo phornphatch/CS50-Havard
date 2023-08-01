a389133fa08774d0e20590922a4002360d1e8aca
ZUMO <phornphatch@gmail.com>
2023-08-01 13:37:31 +0000

    Tue, Aug 1, 2023, 4:37 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,4 +2,14 @@ from cs50 import get_int
 
 height = get_int("Height: ")
 
- while height => 1 or height <= 8
+while height > 1 or height < 8:
+    for i in range(height):
+        print("")
+
+        for j in range(i):
+            print("#")
+
+        print(" ")
+
+        for k in range(i):
+            print("#")
