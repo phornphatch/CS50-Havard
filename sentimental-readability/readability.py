60a0ede4798f76f9d8f41c2f5095838d6f874632
ZUMO <phornphatch@gmail.com>
2023-08-02 13:57:23 +0000

    Wed, Aug 2, 2023, 4:57 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,8 +1,12 @@
-def cal_grade(text):
-    total_letter = 0
-    total_comma = 0
-    total_word = 1  # count by space + 1 to start with 1
-    total_sentence = 0  # count by . or !
+from cs50 import get_string
+
+text = get_string("Text: ")
+actual_grade = 0
+grade = 0
+total_letter = 0
+total_comma = 0
+total_word = 1  # count by space + 1 ot start with 1
+total_sentence = 0  # count by . or !
 
     for character in text:
         if character == ',':
@@ -17,21 +21,20 @@ def cal_grade(text):
 
     L = (total_letter / (total_word - total_comma)) * 100
     S = (total_sentence / (total_word - total_comma)) * 100
-    grade = (0.0588 * L) - (0.296 * S) - 15.8
-    return grade
+    actual_grade = (0.0588 * L) - (0.296 * S) - 15.8
 
-def main():
-    # get text from user
-    text = input("Text: ")
+# print(total_letter)
+# print(total_word)
+# print(total_sentence)
+# print(total_comma)
+# # print(L)
+# print(S)
+# print(actual_grade)
+actual_grade = round(actual_grade)
 
-    # print grade with conditions
-    actual_grade = cal_grade(text)
-    if actual_grade > 16:
-        print("Grade 16+")
-    elif actual_grade < 1:
-        print("Before Grade 1")
-    else:
-        print(f"Grade {int(round(actual_grade))}")
-
-if __name__ == "__main__":
-    main()
+if actual_grade > 16:
+    print("Grade 16+")
+elif actual_grade < 1:
+    print("Before Grade 1")
+else:
+    print(f"Grade {actual_grade}")
