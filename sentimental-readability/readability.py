77ee99abb23e8d2bed27d9170d4c2f9a4383f988
ZUMO <phornphatch@gmail.com>
2023-08-02 12:37:11 +0000

    Wed, Aug 2, 2023, 3:37 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,4 +3,14 @@ from cs50 import get_int
 text = get_int("Text: ")
 L = 0
 S = 0
+actual_grade = 0
 
+#find grade
+
+
+if actual_grade > 16:
+    print("Grade 16+");
+elif actual_grade < 1:
+    print("Before Grade 1");
+else:
+    print(f"Grade {actual_grade}")
