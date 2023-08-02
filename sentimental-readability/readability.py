c7820ee07612c7941c6594a7c85d6c4309cc187d
ZUMO <phornphatch@gmail.com>
2023-08-02 13:11:03 +0000

    Wed, Aug 2, 2023, 4:11 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -19,7 +19,7 @@ for i in range(len(text)):
     if character == ",":
         total_comma += 1
 
-    if (text[i] == "," and text[i + 1] == " ") or character == " ":
+    if (text[i] == "," and text[i + 1] == " "):
         total_word += 1
     elif character == "." or character == "!" or character == ":" or character == "?":
         total_sentence += 1
