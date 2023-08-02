00b1579c49810cf2b5a7379f7bd98bb2f26b1a29
ZUMO <phornphatch@gmail.com>
2023-08-02 13:34:12 +0000

    Wed, Aug 2, 2023, 4:34 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,8 +5,6 @@ actual_grade = 0
 grade = 0
 total_letter = 0
 total_comma = 0
-total_word_comma = 0
-total_word_all = 0
 total_word = 1  # count by space + 1 ot start with 1
 total_sentence = 0  # count by . or !
 
@@ -19,16 +17,14 @@ for i in range(len(text)):
     if character == ",":
         total_comma += 1
 
-    if character == ',' and text[i+1] == ' ':
-        total_word_comma += 1
-    elif character == ' ' or character == ',':
-        total_word_all += 1
+    if character == ',' or character == ' ':
+        total_word += 1
     elif character == "." or character == "!" or character == ":" or character == "?":
         total_sentence += 1
     elif character != "'":
         total_letter += 1
 
-total_word = total_word_all - total_word_comma
+total_word = text.split(" ")
 L = (total_letter // (total_word - total_comma)) * 100
 S = total_sentence // (total_word - total_comma) * 100
 actual_grade = (0.0588 * L) - (0.296 * S) - 15.8
