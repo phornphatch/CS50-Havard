d541b904a1f948f26625bada608ae950b2b6e914
ZUMO <phornphatch@gmail.com>
2023-08-02 14:28:48 +0000

    Wed, Aug 2, 2023, 5:28 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,9 +5,13 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
+    if len(argv) == 2:
+        print(f"hello, {argv[1]}")
+    else:
+        print("hello, world")
 
     # TODO: Read database file into a variable
-    
+
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -43,11 +47,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
