bb0d45a3ba85200a0ae865be80cb4716fa8a45b1
ZUMO <phornphatch@gmail.com>
2023-08-02 13:55:38 +0000

    Wed, Aug 2, 2023, 4:55 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,44 +1,37 @@
-from cs50 import get_string
+def cal_grade(text):
+    total_letter = 0
+    total_comma = 0
+    total_word = 1  # count by space + 1 to start with 1
+    total_sentence = 0  # count by . or !
 
-text = get_string("Text: ")
-actual_grade = 0
-grade = 0
-total_letter = 0
-total_comma = 0
-total_word = 1  # count by space + 1 ot start with 1
-total_sentence = 0  # count by . or !
+    for character in text:
+        if character == ',':
+            total_comma += 1
 
-# find grade
-for i in range(len(text)):
-    character = text[i]
-    # print(character)
+        if character == ',' or character == ' ':
+            total_word += 1
+        elif character == '.' or character == '!' or character == ':' or character == '?':
+            total_sentence += 1
+        elif character != "'":
+            total_letter += 1
 
-    if character == ",":
-        total_comma += 1
-    if character == ',' or character == ' ':
-        total_word += 1
-    elif character == "." or character == "!" or character == ":" or character == "?":
-        total_sentence += 1
-    elif character != "'" and character != "," and character != ";" and character != "-" and character != "?" and character != '"':
-        total_letter += 1
+    L = (total_letter / (total_word - total_comma)) * 100
+    S = (total_sentence / (total_word - total_comma)) * 100
+    grade = (0.0588 * L) - (0.296 * S) - 15.8
+    return grade
 
-total_word = len(text.split(" "))
-L = (total_letter // (total_word - total_comma)) * 100
-S = (total_sentence / (total_word - total_comma)) * 100
-actual_grade = (0.0588 * L) - (0.296 * S) - 15.8
+def main():
+    # get text from user
+    text = input("Text: ")
 
-# print(total_letter)
-# print(total_word)
-# print(total_sentence)
-# print(total_comma)
-# # print(L)
-# print(S)
-# print(actual_grade)
-actual_grade = round(actual_grade)
+    # print grade with conditions
+    actual_grade = cal_grade(text)
+    if actual_grade > 16:
+        print("Grade 16+")
+    elif actual_grade < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {int(round(actual_grade))}")
 
-if actual_grade > 16:
-    print("Grade 16+")
-elif actual_grade < 1:
-    print("Before Grade 1")
-else:
-    print(f"Grade {actual_grade}")
+if __name__ == "__main__":
+    main()
