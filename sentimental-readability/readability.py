31c9fa8b8ec68ed2cff72de4412f802221e4df65
ZUMO <phornphatch@gmail.com>
2023-08-02 13:19:33 +0000

    Wed, Aug 2, 2023, 4:19 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -14,7 +14,7 @@ total_sentence = 0  # count by . or !
 # find grade
 for i in range(len(text)):
     character = text[i]
-    print(character)
+    # print(character)
 
     if character == ",":
         total_comma += 1
@@ -29,13 +29,13 @@ for i in range(len(text)):
 L = total_letter // (total_word - total_comma) * 100
 S = total_sentence // (total_word - total_comma) * 100
 actual_grade = (0.0588 * L) - (0.296 * S) - 15.8
-print(total_letter)
-print(total_sentence)
-print(total_word)
-print(total_comma)
-print(L)
+# print(total_letter)
+# print(total_sentence)
+# print(total_word)
+# print(total_comma)
+# print(L)
 print(S)
-print(actual_grade)
+# print(actual_grade)
 
 if actual_grade > 16:
     print("Grade 16+")
