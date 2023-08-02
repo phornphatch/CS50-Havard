c44b8abb81804370f5f1a8a438500e7c75cf7c5c
ZUMO <phornphatch@gmail.com>
2023-08-02 12:40:35 +0000

    Wed, Aug 2, 2023, 3:40 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,12 +1,20 @@
-from cs50 import get_int
+from cs50 import get_string
 
-text = get_int("Text: ")
+text = get_string("Text: ")
 L = 0
 S = 0
 actual_grade = 0
 
 #find grade
+for i in range(len(text)):
+    if i == ","
+        total_comma += 1
 
+    if character == ',' or character == ' ':
+        total_word += 1
+    elif character == '.' or character == '!' or character == ':' or character == '?':
+        total_sentence += 1
+    else: 
 
 if actual_grade > 16:
     print("Grade 16+");
