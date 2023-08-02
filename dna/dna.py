4ab1d0272f070957e4dad0a8a6393db0100fe458
ZUMO <phornphatch@gmail.com>
2023-08-02 14:43:12 +0000

    Wed, Aug 2, 2023, 5:43 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -11,8 +11,8 @@ def main():
     # TODO: Read database file into a variable
     with open(sys.argv[1]) as file:
         file_reader = csv.DictReader(file)
-        for data in file_reader
-            
+
+
 
 
     # TODO: Read DNA sequence file into a variable
