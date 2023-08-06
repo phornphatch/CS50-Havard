6b0b00a3ed95edcde9bda3437b6c024a41a3821a
ZUMO <phornphatch@gmail.com>
2023-08-06 12:27:10 +0000

    Sun, Aug 6, 2023, 3:27 PM +03:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,2 +1,5 @@
 UPDATE shows SET title = 'The Office' where title like 'the office';
 UPDATE shows SET title = 'Adventure Time' where title like 'adventure time';
+UPDATE shows SET title = 'Arrow' where title like 'arrow';
+UPDATE shows SET title = 'Avatar: The Last Airbender' where title like 'avatar %';
+UPDATE shows SET title = 'Brooklyn Nine-Nine' where title like 'brooklyn%';
