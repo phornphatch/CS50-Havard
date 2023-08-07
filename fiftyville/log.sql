4d83da99b40b1bfe8b067ecd0a23a5aba6e702d6
ZUMO <phornphatch@gmail.com>
2023-08-07 12:33:18 +0000

    Mon, Aug 7, 2023, 3:33 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,4 +11,5 @@ SELECT * FROM interviews WHERE month = 7 AND day = 28 AND year = 2021 AND transc
 
 -- According to Ruth interviews `the thief get into a car in the bakery parking lot and drive away`
 -- after 10:15am, exit
-
+SELECT * FROM bakery_security_logs WHERE month = 7 AND day = 28 AND year = 2021 AND hour = 10 AND minute > 15 AND minute < 25 AND activity =
+"exit";
