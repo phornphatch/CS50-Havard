aca1970bd943b8e7fa2add86d63f2e2a8b6c7255
ZUMO <phornphatch@gmail.com>
2023-08-07 12:24:29 +0000

    Mon, Aug 7, 2023, 3:24 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,5 @@
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- id = 295
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+
+SELECT * FROM interviews WHERE month = 7 AND day = 28 AND year = 2021;
