49b0753a347d29d5251302721f6d5c2563ba8322
ZUMO <phornphatch@gmail.com>
2023-08-07 12:19:59 +0000

    Mon, Aug 7, 2023, 3:19 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- id = 295
