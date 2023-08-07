dd1209e8841ecc5282ad3a32c771dc6e044220f9
ZUMO <phornphatch@gmail.com>
2023-08-07 12:56:52 +0000

    Mon, Aug 7, 2023, 3:56 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,9 +11,10 @@ SELECT * FROM interviews WHERE month = 7 AND day = 28 AND year = 2021 AND transc
 
 -- According to Ruth interviews `the thief get into a car in the bakery parking lot and drive away`
 -- after 10:15am, exit
-SELECT * FROM bakery_security_logs
+SELECT name FROM bakery_security_logs
     JOIN people ON bakery_security_logs.license_plate = people.license_plate
     WHERE month = 7 AND day = 28 AND year = 2021 AND hour = 10 AND minute > 15 AND minute < 25 AND activity = "exit";
+--  Vanessa | Bruce| Barry| Luca | Sofia| Iman | Diana| Kelsey
 -- +-----+------+-------+-----+------+--------+----------+---------------+--------+---------+----------------+-----------------+---------------+
 -- | id  | year | month | day | hour | minute | activity | license_plate |   id   |  name   |  phone_number  | passport_number | license_plate |
 -- +-----+------+-------+-----+------+--------+----------+---------------+--------+---------+----------------+-----------------+---------------+
@@ -30,11 +31,11 @@ SELECT * FROM bakery_security_logs
 
 -- According to Eugene interviews
 -- `Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.`
-SELECT * FROM atm_transactions
+SELECT name FROM atm_transactions
     JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
     JOIN people ON people.id = bank_accounts.person_id
     WHERE month = 7 AND day = 28 AND year = 2021 AND atm_location = "Leggett Street" AND transaction_type = "withdraw";
-
+-- Bruce / | Diana / | Brooke X | Kenny X | Iman / | Luca / | Taylor X | Benista X
 -- +-----+----------------+------+-------+-----+----------------+------------------+--------+----------------+-----------+---------------+--------+---------+----------------+-----------------+---------------+
 -- | id  | account_number | year | month | day |  atm_location  | transaction_type | amount | account_number | person_id | creation_year |   id   |  name   |  phone_number  | passport_number | license_plate |
 -- +-----+----------------+------+-------+-----+----------------+------------------+--------+----------------+-----------+---------------+--------+---------+----------------+-----------------+---------------+
@@ -47,3 +48,20 @@ SELECT * FROM atm_transactions
 -- | 266 | 76054385       | 2021 | 7     | 28  | Leggett Street | withdraw         | 60     | 76054385       | 449774    | 2015          | 449774 | Taylor  | (286) 555-6063 | 1988161715      | 1106N58       |
 -- | 313 | 81061156       | 2021 | 7     | 28  | Leggett Street | withdraw         | 30     | 81061156       | 438727    | 2018          | 438727 | Benista | (338) 555-6650 | 9586786673      | 8X428L0       |
 -- +-----+----------------+------+-------+-----+--------------
+
+-- According to Raymond interviews
+-- `As the thief was leaving the bakery, they called someone who talked to them for less than a minute. `
+SELECT name AS caller_name FROM phone_calls
+    JOIN people ON people.phone_number = phone_calls.caller
+    WHERE month = 7 AND day = 28 AND year = 2021 AND duration < 60;
+-- Caller_name: Sofia X | Kelsey X | Bruce / | Kelsey X | Taylor X | Diana /  | Carina X | Kenny X | Benista  X |
+
+SELECT name AS receiver_name FROM phone_calls
+    JOIN people ON people.phone_number = phone_calls.receiver
+    WHERE month = 7 AND day = 28 AND year = 2021 AND duration < 60;
+-- Caller_name: Jack   | Larry  | Robin  | Melissa| James  | Philip | Jacqueline    |Doris  | Anna
+
+-- According to Raymond interviews
+-- `In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket.`
+SELECT * FROM flights
+    WHERE month = 7 AND day = 29 AND year = 2021;
