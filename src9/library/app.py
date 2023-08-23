7f1ba71d007271d456ec1e3517db48deac351f88
ZUMO <phornphatch@gmail.com>
2023-08-23 12:50:43 +0000

    Wed, Aug 23, 2023, 3:50 PM +03:00

--- src9/library/app.py
+++ src9/library/app.py
@@ -10,7 +10,9 @@ db = SQL("sqlite:///history.db") # connect bd by using sqlite
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-@app.route("/", methods=["GET"]) # / is rootfolder, homepage, main page
+@app.route("/", methods=["GET", "POST"]) # / is rootfolder, homepage, main page
 def index():
+    if request.method == "POST":
+        print("Submit from POST !")
     string = random_string(1000)
     return render_template("index.html", random_string=string, name="ZUMO")
