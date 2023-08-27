3d42720fb837391556a38346f8b2784591714fcf
ZUMO <phornphatch@gmail.com>
2023-08-27 14:57:12 +0000

    Sun, Aug 27, 2023, 5:57 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html (Person name and Person birthday)
-        shows = db.execute("SELECT * FROM birthdays)
+        birthdays = db.execute("SELECT * FROM birthdays)
         return render_template("index.html")
 
 
