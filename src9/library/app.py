cf988b58ccf4fc34d372d408a0fce2c04d47446b
ZUMO <phornphatch@gmail.com>
2023-08-27 12:48:52 +0000

    Sun, Aug 27, 2023, 3:48 PM +03:00

--- src9/library/app.py
+++ src9/library/app.py
@@ -13,6 +13,19 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/", methods=["GET", "POST"]) # / is rootfolder, homepage, main page
 def index():
     if request.method == "POST":
-        print("Submit from POST !")
+        page = request.form.get("page")
+
+        try:
+            page = int(page)
+        except ValueError:
+            return render_template("index.html", random_string="Enter a Number !")
+
+        if page < 0:
+            return render_template("index.html", random_string="Type positive number !")
+
+        db.execute("INSERT INTO history (page) VALUES (?)", page) # *** เขียน sql ใน python ใช้ db.execute ***
+        random.seed(page) # ทำให้ ถ้าหน้าเดิม random string จะออกมาเหมือนเดิม
+
     string = random_string(1000)
-    return render_template("index.html", random_string=string, name="ZUMO")
+    rows = db.execute("SELECT * FROM history;")
+    return render_template("index.html", random_string=string, name="ZUMO", data=rows)
