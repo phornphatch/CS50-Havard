1a2983f3d17babab9103dcda5c935dbebfb9f242
ZUMO <phornphatch@gmail.com>
2023-08-27 16:16:55 +0000

    Sun, Aug 27, 2023, 7:16 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -109,6 +109,11 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username"):
+        password = request.form.get("password"):
+        confirmation = request.form.get("confirmation"):
+    else:
     return apology("TODO")
 
 
