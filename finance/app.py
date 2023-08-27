a05ec5f1d1edd7ec87843795213d4c804c4afcc4
ZUMO <phornphatch@gmail.com>
2023-08-27 16:33:56 +0000

    Sun, Aug 27, 2023, 7:33 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -130,10 +130,10 @@ def register():
         elif not password == confirmation:
             return apology("passwords do not match", 403)
 
-        db.execute("INSERT INTO users (username, password) VALUES(?, ?)", username, password)
+        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, werkzeug.security.generate_password_hash(password, method='pbkdf2', salt_length=16))
         print("Successful Register !")
         return redirect("/")
-    
+
     else:
         return render_template("register.html")
 
